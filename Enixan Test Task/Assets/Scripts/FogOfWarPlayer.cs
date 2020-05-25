390bb3d0e672883406d514b627f40a2099acc856
AndrewNepliakh <father.of.people@gmail.com>
2020-05-25 21:03:39 +0300

    Init commit

---
+++ Enixan Test Task/Assets/Scripts/FogOfWarPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogOfWarPlayer : MonoBehaviour
+{
+
+	public Transform FogOfWarPlane;
+	public int Number = 1;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Ray rayToPlayerPos = Camera.main.ScreenPointToRay(transform.position);
+		int layermask = 1 << 8;
+		RaycastHit hit;
+		if (Physics.Raycast(rayToPlayerPos, out hit, 1000, layermask))
+		{
+			FogOfWarPlane.GetComponent<Renderer>().material.SetVector("_Player" + Number.ToString() + "_Pos", hit.point);
+		}
+	}
+}
